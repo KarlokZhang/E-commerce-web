291883c84cf65be2da712fd1aacfe1f6efc6b9c8
KarlokCheung <georgecheung07@gmail.com>
2021-02-12 16:11:46 +1100

    add HomePage with initial structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import HomePage from "./homepage.component";
 
 class App extends Component {
   render() {
-    return <div className='App'>Hello World</div>;
+    return <div className="App">Hello World</div>;
   }
 }
 
