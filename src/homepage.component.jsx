b4985c9a93697b1d700537aa1922704e30736c45
KarlokCheung <georgecheung07@gmail.com>
2021-02-12 17:02:14 +1100

    refactor homepage styles

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -6,36 +6,36 @@ const HomePage = () => (
     <div className="directory-menu">
       <div className="menu-item">
         <div className="content">
-          <h1 className="title">HAT</h1>
+          <div className="title">HATS</div>
           <span className="subtitle">SHOP NOW</span>
         </div>
+      </div>
 
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">JACKETS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
+      <div className="menu-item">
+        <div className="content">
+          <div className="title">JACKETS</div>
+          <span className="subtitle">SHOP NOW</span>
         </div>
+      </div>
 
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">SNEAKERS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
+      <div className="menu-item">
+        <div className="content">
+          <div className="title">SNEAKERS</div>
+          <span className="subtitle">SHOP NOW</span>
         </div>
+      </div>
 
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">WOMENS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
+      <div className="menu-item">
+        <div className="content">
+          <div className="title">WOMENS</div>
+          <span className="subtitle">SHOP NOW</span>
         </div>
+      </div>
 
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">MENS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
+      <div className="menu-item">
+        <div className="content">
+          <div className="title">MENS</div>
+          <span className="subtitle">SHOP NOW</span>
         </div>
       </div>
     </div>
