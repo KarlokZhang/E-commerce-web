0991f28a70f3bfa0dd9352ed8a648a8bbaccfc36
KarlokCheung <georgecheung07@gmail.com>
2021-02-12 21:54:24 +1100

    Add directory component

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,16 +1,10 @@
 import React from "react";
-import MenuItem from "../../components/menu-item/menu-item.component";
 import "./homepage.styles.scss";
+import Directory from "../../components/directory/directory.component";
 
 const HomePage = () => (
   <div className="homepage">
-    <div className="directory-menu">
-      <MenuItem title="HATS" />
-      <MenuItem title="JACKETS" />
-      <MenuItem title="SNEAKERS" />
-      <MenuItem title="WOMENS" />
-      <MenuItem title="MENS" />
-    </div>
+    <Directory />
   </div>
 );
 
