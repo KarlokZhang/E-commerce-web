ff0fdd4d3294c52dac5880ba8d91fd86c70fcb32
KarlokCheung <georgecheung07@gmail.com>
2021-02-13 15:13:58 +1100

    bug fixed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React, { Component } from "react";
 import "./App.css";
-import HomePage from "./pages/homepage/homepage.component";
+import HomePage from "./homepage.component";
 
 class App extends Component {
   render() {
-    return (
-      <div>
-        <HomePage />
-      </div>
-    );
+    return <div className="App">Hello World</div>;
   }
 }
 
---
+++ src/homepage.component.jsx
@@ -0,0 +1,44 @@
+import React from "reactf";
+
+const HomePage = () => (
+  <div className="homepage">
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">HAT</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">JACKETS</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">SNEAKERS</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">WOMENS</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">MENS</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default HomePage;
