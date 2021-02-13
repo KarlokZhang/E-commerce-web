8d1c034a608cb74cae5a2b803eb6fb470fabf6fd
KarlokCheung <georgecheung07@gmail.com>
2021-02-13 15:24:34 +1100

    Revert "bug fixed"
    This reverts commit ff0fdd4d3294c52dac5880ba8d91fd86c70fcb32.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
-import HomePage from "./homepage.component";
+import HomePage from "./pages/homepage/homepage.component";
 
 class App extends Component {
   render() {
-    return <div className="App">Hello World</div>;
+    return (
+      <div>
+        <HomePage />
+      </div>
+    );
   }
 }
 
