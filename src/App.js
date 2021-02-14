67417a3b172107157babe873d444b1fb186e5424
KarlokCheung <georgecheung07@gmail.com>
2021-02-14 20:24:46 +1100

    add Router for hats page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from "react";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+import { Switch, Route } from "react-router-dom";
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE </h1>
+  </div>
+);
 
 class App extends Component {
   render() {
     return (
       <div>
-        <HomePage />
+        <Switch>
+          <Route exact path="/" component={HomePage}></Route>
+          <Route path="/hats" component={HatsPage}></Route>
+        </Switch>
       </div>
     );
   }
