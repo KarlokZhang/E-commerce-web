41f3ff3d6dc6d9ebabe79e5790b72b96e0721721
KarlokCheung <georgecheung07@gmail.com>
2021-02-20 22:04:41 +1100

    Add route, Link, Switch Demonstration

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
 import React, { Component } from "react";
 import "./App.css";
-import HomePage from "./pages/homepage/homepage.component";
-import { Switch, Route } from "react-router-dom";
+import { Link, Route } from "react-router-dom";
 
-const HatsPage = () => (
+const HomePage = (props) => (
   <div>
-    <h1>HATS PAGE </h1>
+    <button onClick={() => props.history.push("/topics")} >Topics</button>
+    <h1>HOME PAGE</h1>
   </div>
 );
+
+const TopicsList = (props) => (
+  <div>  
+    <h1>TOPIC LIST PAGE</h1>
+    <Link to={`${props.match.url}/13`} >TO TOPIC 13</Link>
+    <Link to={`${props.match.url}/17`} >TO TOPIC 17</Link>
+    <Link to={`${props.match.url}/21`} >TO TOPIC 21</Link>
+  </div>
+);
+
+const TopicDetail = (props) => {
+  return (
+    <div>
+      <h1>TOPIC DETAIL PAGE: {props.match.params.topicID}</h1>
+    </div>
+  )
+}
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Switch>
           <Route exact path="/" component={HomePage}></Route>
-          <Route path="/hats" component={HatsPage}></Route>
-        </Switch>
+          <Route exact path="/topics" component={TopicsList}></Route>
+          <Route path="/topics/:topicID" component={TopicDetail}></Route>
       </div>
     );
   }
