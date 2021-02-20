0c0a1dde0c8afeb50218b40ef1e4216576e57fee
KarlokCheung <georgecheung07@gmail.com>
2021-02-20 22:20:50 +1100

    add withRouter in menu-item

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
-import { Link, Route } from "react-router-dom";
+import { Route } from "react-router-dom";
+import HomePage from "./pages/homepage/homepage.component";
 
-const HomePage = (props) => (
-  <div>
-    <button onClick={() => props.history.push("/topics")} >Topics</button>
-    <h1>HOME PAGE</h1>
-  </div>
-);
 
-const TopicsList = (props) => (
-  <div>  
-    <h1>TOPIC LIST PAGE</h1>
-    <Link to={`${props.match.url}/13`} >TO TOPIC 13</Link>
-    <Link to={`${props.match.url}/17`} >TO TOPIC 17</Link>
-    <Link to={`${props.match.url}/21`} >TO TOPIC 21</Link>
-  </div>
-);
-
-const TopicDetail = (props) => {
+const HatsPage = () => {
   return (
     <div>
-      <h1>TOPIC DETAIL PAGE: {props.match.params.topicID}</h1>
+      <h1>HATS PAGE </h1>
     </div>
   )
 }
@@ -31,8 +17,7 @@ class App extends Component {
     return (
       <div>
           <Route exact path="/" component={HomePage}></Route>
-          <Route exact path="/topics" component={TopicsList}></Route>
-          <Route path="/topics/:topicID" component={TopicDetail}></Route>
+          <Route path="/hats" component={HatsPage}></Route>
       </div>
     );
   }
