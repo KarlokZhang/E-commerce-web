ccf20f164742f4a00e2caa05b0e3d5376cec0782
KarlokCheung <georgecheung07@gmail.com>
2021-02-22 23:40:17 +1100

    fix number of collection preview

--- src/components/preview-collection/collection-preview.component.jsx
+++ src/components/preview-collection/collection-preview.component.jsx
@@ -4,9 +4,11 @@ const CollectionPreview = ({ title, items }) => (
   <div className="collection-preview">
     <h1 className="title">{title.toUpperCase()}</h1>
     <div className="preview">
-      {items.map((item) => (
-        <div key={item.id}>{item.name}</div>
-      ))}
+      {items
+        .filter((item, idx) => idx < 4)
+        .map((item) => (
+          <div key={item.id}>{item.name}</div>
+        ))}
     </div>
   </div>
 );
