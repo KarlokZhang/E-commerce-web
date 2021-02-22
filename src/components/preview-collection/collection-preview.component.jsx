79feb5d067278a07f4b844d5e9f773efc6c558f0
KarlokCheung <georgecheung07@gmail.com>
2021-02-22 23:38:25 +1100

    add collection preview page

---
+++ src/components/preview-collection/collection-preview.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const CollectionPreview = ({ title, items }) => (
+  <div className="collection-preview">
+    <h1 className="title">{title.toUpperCase()}</h1>
+    <div className="preview">
+      {items.map((item) => (
+        <div key={item.id}>{item.name}</div>
+      ))}
+    </div>
+  </div>
+);
+
+export default CollectionPreview;
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 import SHOP_DATA from "./shop.data";
+import CollectionPreview from "../../components/preview-collection/collection-preview.component";
 
 class ShopPage extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      collection: SHOP_DATA,
+      collections: SHOP_DATA,
     };
   }
   render() {
-    return <div>SHOP PAGE</div>;
+    const { collections } = this.state;
+    return (
+      <div className="shop-page">
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
+      </div>
+    );
   }
 }
 
