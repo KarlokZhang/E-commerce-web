f8b3a814322d0d9a8fda6e2bb0ad70e2be8fee41
KarlokCheung <georgecheung07@gmail.com>
2021-02-28 22:01:35 +1100

    add sign-up component and styles

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -7,7 +7,7 @@ import { auth, createUserProfileDocument } from '../../firebase/firebase.utils';
 
 import './sign-up.styles.scss';
 
-class SignUp extends Component {
+class SignUp extends React.Component {
   constructor() {
     super();
 
