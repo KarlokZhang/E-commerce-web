cbac5f152fb5c272fc50767bd9c0d959d2eeb550
KarlokCheung <georgecheung07@gmail.com>
2021-03-02 22:23:34 +1100

    add root and user reducer

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-import * as serviceWorker from "./serviceWorker";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { provider } from 'react-redux';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById("root")
+  <Provider>
+    <BrowserRouter>
+     <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
 );
