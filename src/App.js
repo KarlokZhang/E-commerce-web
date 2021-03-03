d32b228fa70ae518a973e9d237d957117e573fdf
KarlokCheung <georgecheung07@gmail.com>
2021-03-03 22:32:54 +1100

    remove unused component

--- src/App.js
+++ src/App.js
@@ -6,11 +6,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import {
-  auth,
-  createUserProfileDocument,
-  signInWithGoogle,
-} from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
 class App extends Component {
   unsubscribeFromAuth = null;
