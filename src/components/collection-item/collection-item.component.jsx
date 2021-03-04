f2834a273b20e399f8a9bc7e8fe2807e461146aa
KarlokCheung <georgecheung07@gmail.com>
2021-03-04 22:16:40 +1100

    add add to cart on clooection items

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,19 +1,21 @@
-import React from "react";
+import React from 'react';
+import CustomButton from '../custom-button/custom-button.component';
 
-import "./collection-item.styles.scss";
+import './collection-item.styles.scss';
 
 const CollectionItem = ({ id, name, price, imageUrl }) => (
-  <div className="collection-item">
+  <div className='collection-item'>
     <div
-      className="image"
+      className='image'
       style={{
         backgroundImage: `url(${imageUrl})`,
       }}
     ></div>
-    <div className="collection-footer">
-      <span className="name">{name}</span>
-      <span className="price">{price}</span>
+    <div className='collection-footer'>
+      <span className='name'>{name}</span>
+      <span className='price'>{price}</span>
     </div>
+    <CustomButton inverted> Add to cart </CustomButton>
   </div>
 );
 
