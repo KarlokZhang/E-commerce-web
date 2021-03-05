1d9ac83356e1a3c8b5e491e2448c1ea2099d546d
KarlokCheung <georgecheung07@gmail.com>
2021-03-05 22:03:25 +1100

    add card item into dropdown and connect cart dropdown with cart reducer

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -5,7 +5,7 @@ import './cart-item.styles.scss';
 const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
   <div className='cart-item'>
     <img src={imageUrl} alt='item' />
-    <div className='item-detail'>
+    <div className='item-details'>
       <span className='name'>{name}</span>
       <span className='price'>
         {quantity} x ${price}
