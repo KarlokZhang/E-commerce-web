53dbdd8a66423321bfa615a6752c6a9d0f26394f
KarlokCheung <georgecheung07@gmail.com>
2021-04-01 22:28:41 +1100

    add Stripe Button component

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    'pk_test_51IbOQAIZQlzqEow7GeBts0kyXUE0m5nr1Ps2GWPZBAjhKZrCdm1wlBiZVTtygv76ZljjJG1BKxplYhTgO7xRuX5q00FBD9eqkL';
+
+  const onToken = (token) => {
+    console.log(token);
+    alert('Payment Successful');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now '
+      name='CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
+
