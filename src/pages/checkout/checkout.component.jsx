a3b76d5d344308a49332ef034c74f07a561600ac
KarlokCheung <georgecheung07@gmail.com>
2021-04-01 22:51:32 +1100

    add stripe checkout button

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -6,6 +6,7 @@ import {
   selectCartItems,
   selectCartTotal,
 } from '../../redux/cart/cart.selectors';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 
 import './checkout.styles.scss';
 
@@ -35,6 +36,12 @@ const CheckoutPage = ({ cartItems, total }) => (
     <div className='total'>
       <span>TOTAL: ${total}</span>
     </div>
+    <div className="test-warning">
+      *Please use the following test credit card for payments*
+      <br/>
+      4242 4242 4242 4242 - Exp: 12/23 - CVV: 123
+    </div>
+    <StripeCheckoutButton price={total} />
   </div>
 );
 
