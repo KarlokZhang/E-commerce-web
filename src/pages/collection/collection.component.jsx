54b7125be6d734485790436743684d92e4d9c353
KarlokCheung <georgecheung07@gmail.com>
2021-04-01 14:48:26 +1100

    refactor shop date from array to object

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -7,14 +7,22 @@ import { selectCollection } from '../../redux/shop/shop.selector';
 
 import './collection.styles.scss';
 
-const CollectionPage = ({ match }) => (
-  <div className='category'>
-    <h2>COLLECTION PAGE</h2>
-  </div>
-);
+const CollectionPage = ({ collection }) => {
+  const { title, items } = collection;
+  return (
+    <div className='collection-page'>
+      <h2 className='title'>{title}</h2>
+      <div className='items'>
+        {items.map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 const mapStateToProps = (state, ownProps) => ({
-  collection: selectCollection(ownProps.match.params.collectionID)(state)
-})
+  collection: selectCollection(ownProps.match.params.collectionId)(state),
+});
 
 export default connect(mapStateToProps)(CollectionPage);
