fa0bd74761bf2e4b77bf6ba6c84289200e9bf5da
KarlokCheung <georgecheung07@gmail.com>
2021-06-02 22:27:53 +1000

    add method to add collection to firestore

--- src/App.js
+++ src/App.js
@@ -7,15 +7,16 @@ import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import CheckoutPage from './pages/checkout/checkout.component';
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument, addCollectionAndDocuments } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
 import { createStructuredSelector } from 'reselect';
+import { selectCollectionsForPreview } from './redux/shop/shop.selector';
 class App extends Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    const { setCurrentUser } = this.props;
+    const { setCurrentUser, collectionArray } = this.props;
 
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
@@ -30,6 +31,7 @@ class App extends Component {
       }
 
       setCurrentUser(userAuth);
+      addCollectionAndDocuments('collections', collectionArray);
     });
   }
 
@@ -64,6 +66,7 @@ class App extends Component {
 
 const mpaStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser,
+  collectionArray: selectCollectionsForPreview
 });
 
 const mapDispatchToProps = (dispatch) => ({
