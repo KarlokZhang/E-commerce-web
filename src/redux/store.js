4ee9e3084c12a0e4053d5a36e9a10818818e24f4
KarlokCheung <georgecheung07@gmail.com>
2021-06-09 22:38:00 +1000

    add logger in middlewares

--- src/redux/store.js
+++ src/redux/store.js
@@ -7,6 +7,10 @@ import rootReducer from './root-reducer';
 
 const middlewares = [thunk];
 
+if(process.env.NODE_ENV === 'development'){
+  middlewares.push(logger);
+}
+
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 export const persistor = persistStore(store);
 
