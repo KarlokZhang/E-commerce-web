0f6d294ca4e35a3f83be1c611238fa992d61d205
KarlokCheung <georgecheung07@gmail.com>
2021-06-09 22:54:55 +1000

    add shop actions for success, failure, start and start async

--- src/redux/shop/shop.action.js
+++ src/redux/shop/shop.action.js
@@ -1,7 +1,32 @@
 import ShopActionTypes from './shop.types';
+import {
+  firestore,
+  convertCollectionsSnapshotToMap,
+} from '../../firebase/firebase.utils';
 
-export const updateCollections = collectionMap => ({
-  type: ShopActionTypes.UPDATE_COLLECTIONS,
-  payload: collectionMap
+export const fetchCollectionStart = () => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_START,
 });
 
+export const fetchCollectionStartAsync = () => {
+  return (dispatch) => {
+    const collectionRef = firestore.collection('collections');
+    dispatch(fetchCollectionStart());
+
+    collectionRef.get().then((snapshot) => {
+      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+      dispatch(fetchCollectionSuccess(collectionsMap));
+      this.setState({ loading: false }); 
+    }).catch(error => dispatch(fetchCollectionFailure(error.message)));
+  };
+};
+
+export const fetchCollectionSuccess = (collectionsMap) => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_SUCCESS,
+  payload: collectionsMap
+});
+
+export const fetchCollectionFailure = (errorMessage) => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
+  payload: errorMessage
+});
